7b4bebcf5f665b5e63e5fdc14267487e13da42a3
King Julian <daniel.bradley09@gmail.com>
2021-01-23 15:24:58 -0700

    rough draft of index

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 //const { generateMarkdown } = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const promptUser = () => {
-    return inquirer.prompt([
+const questions = [
         {
             type: 'input',
             name: 'title',
@@ -22,8 +21,8 @@ const promptUser = () => {
             type: 'input',
             name: 'description',
             message: 'Describe your project. (Required)',
-            validate: projectInput => {
-              if (projectInput) {
+            validate: descriptionInput => {
+              if (descriptionInput) {
                 return true;
               } else {
                 console.log('Please enter a project description!');
@@ -32,28 +31,47 @@ const promptUser = () => {
             }
           },
           {
-            type: 'confirm',
-            name: 'confirmInstallation',
-            message: 'Would you like to enter information about the installation of your project?',
-            default: true
-          },
-          {
             type: 'input',
             name: 'installation',
-            message: 'Describe how to install your project:',
-            when: ({ confirmInstallation }) => confirmInstallation
-          }
-          
-    ])
-}
+            message: 'Provide any information for the intallation of your project:',
+          },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide any usage information for the project:'
+        },
+        {
+           type: 'input',
+           name: 'contributing',
+           message: 'Who contributed to this project?'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Provide any tests written for the application and how they are run:'
+        }
+            // put licenses here!!!   
+    ];
+
 
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+
+    console.log("Your Readme file is ready!")
+    });
+}
+        
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions) 
+}
 
 // Function call to initialize app
 init();
